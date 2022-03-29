2fe8166260086890715f14b203213de33ae4720c
J.Z. Bradley <jzbradley@gmail.com>
2022-03-29 16:22:39 -0700

    +NotNull

--- InternalExtensions.cs
+++ InternalExtensions.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 
@@ -12,4 +13,24 @@ internal static partial class InternalExtensions
 
     public static HashSet<T> ToHashSet<T>(this IEnumerable<T> @this)
         => new HashSet<T>(@this);
+
+    public static IEnumerable<T> NotNull<T>(this IEnumerable<T?> @this) where T : class
+    {
+        foreach (var item in @this)
+        {
+            if (item== null) continue;
+            yield return item;
+        }
+    }
+    public static IEnumerable<TResult> NotNull<TSource, TResult>(this IEnumerable<TSource?> @this, Func<TSource?, TResult?>predicate)
+        where TSource : class
+        where TResult : class
+    {
+        foreach (var source in @this)
+        {
+            var result = predicate(source);
+            if (result== null) continue;
+            yield return result;
+        }
+    }
 }
