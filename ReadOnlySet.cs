cad2a1562f13e9f65b0dfda702bc195cfb70520e
J.Z. Bradley <jzbradley@gmail.com>
2022-03-29 15:28:39 -0700

    +IReadOnlySet

--- ReadOnlySet.cs
+++ ReadOnlySet.cs
@@ -72,3 +72,26 @@ public class ReadOnlySet<T> : IReadOnlySet<T>
         return _set.SetEquals(other);
     }
 }
+
+public interface IReadOnlySet<T> : IReadOnlyCollection<T>
+{
+    void UnionWith(IEnumerable<T> other);
+
+    void IntersectWith(IEnumerable<T> other);
+
+    void ExceptWith(IEnumerable<T> other);
+
+    void SymmetricExceptWith(IEnumerable<T> other);
+
+    bool IsSubsetOf(IEnumerable<T> other);
+
+    bool IsSupersetOf(IEnumerable<T> other);
+
+    bool IsProperSupersetOf(IEnumerable<T> other);
+
+    bool IsProperSubsetOf(IEnumerable<T> other);
+
+    bool Overlaps(IEnumerable<T> other);
+
+    bool SetEquals(IEnumerable<T> other);
+}
